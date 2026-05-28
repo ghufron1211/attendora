b6ef0d40804131ae4f361c516c2e20b4f25b2f33
Abdul Rauf Ghufron <abdulraufghufron@gmail.com>
2026-05-28 18:09:30 +0700

    remove redirectGuestsTo

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,11 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+
         $middleware->alias([
             'role' => RoleMiddleware::class,
         ]);
 
-        $middleware->redirectGuestsTo('/login');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
